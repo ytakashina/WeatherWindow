7f845fd4890698573bf92542fab74413139630bb
ytakashina <takashina2051@gmail.com>
2019-03-12 13:51:12 +0900

    geolocation feature

--- Assets/WeatherFetcher.cs
+++ Assets/WeatherFetcher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.IO;
@@ -8,27 +9,48 @@ using UnityEngine;
 
 public class WeatherFetcher : MonoBehaviour
 {
-    public string ApiKey = "774233a13a975bc8fcd4b6e894482ce9";
+    public string ApiKeyWeather = "774233a13a975bc8fcd4b6e894482ce9";
+    public string ApiKeyGeoIp = "420786de36a519f50bdd36ea19c3d8d1a470ace60a6a8e764ac70e20";
     public string City = "Tokyo";
     public string Country = "Japan";
+    public string Weather;
+    public string Latitude;
+    public string Longtitude;
+
+    string SendHttpRequest(string uri)
+    {
+        var request = (HttpWebRequest) WebRequest.Create(uri);
+        using (var response = (HttpWebResponse) request.GetResponse())
+        using (var stream = response.GetResponseStream())
+        using (var reader = new StreamReader(stream))
+        {
+            if ((int) response.StatusCode != 200)
+            {
+                throw new Exception($"Bad status code: {response.StatusCode}.");
+            }
+            return reader.ReadToEnd();
+        }
+    }
 
     // Use this for initialization
     void Start()
     {
-        var url = $"http://api.openweathermap.org/data/2.5/weather?q={City},{Country}&appid={ApiKey}";
-        var request = (HttpWebRequest) WebRequest.Create(url);
-        using (var response = (HttpWebResponse)request.GetResponse())
-        using (var stream = response.GetResponseStream())
-        using (var reader = new StreamReader(stream))
-        {
-            var json = reader.ReadToEnd();
-            var dict = JObject.Parse(json);
-            Debug.Log(dict["weather"][0]["id"]);
-            Debug.Log(dict["weather"][0]["main"]);
-        }
+        var uriGeoIp = $"https://api.ipdata.co/?api-key={ApiKeyGeoIp}";
+        var jsonGeoIp = SendHttpRequest(uriGeoIp);
+        var dictGeoIp = JObject.Parse(jsonGeoIp);
+        Latitude = (string) dictGeoIp["latitude"];
+        Longtitude = (string) dictGeoIp["longtitude"];
+
+        Debug.Log("executed");
+
+//        var uriWeather = $"http://api.openweathermap.org/data/2.5/weather?q={City},{Country}&appid={ApiKeyWeather}";
+        var uriWeather = $"http://api.openweathermap.org/data/2.5/weather?lat={Latitude}&lon={Longtitude}&appid={ApiKeyWeather}";
+        var jsonWeather = SendHttpRequest(uriWeather);
+        var dictWeather = JObject.Parse(jsonWeather);
+        Weather = (string) dictWeather["weather"][0]["main"];
     }
 
 
-// Update is called once per frame
+    // Update is called once per frame
     void Update() { }
 }
