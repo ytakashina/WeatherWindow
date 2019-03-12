5c7efbc3948e4ae47bba8206e72a0125800f318e
ytakashina <takashina2051@gmail.com>
2019-03-12 23:45:18 +0900

    Deleted unnecessary plugins & prototyped window

---
+++ Assets/WeatherWindow/WindowController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowController : MonoBehaviour
+{
+    public WeatherFetcher WeatherFetcher;
+    public Material GlassMaterial;
+    public Texture TextureClearDay;
+    public Texture TextureCloudsDay;
+    public Texture TextureRainDay;
+
+    // Use this for initialization
+    void Start()
+    {
+        WeatherFetcher = FindObjectOfType<WeatherFetcher>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (WeatherFetcher.Weather.ToLower())
+        {
+            case "clear":
+                GlassMaterial.SetTexture("_MainTex", TextureClearDay);
+                break;
+            case "clouds":
+            case "atmosphere":
+                GlassMaterial.SetTexture("_MainTex", TextureCloudsDay);
+                break;
+            case "rain":
+            case "snow":
+            case "drizzle":
+            case "thunderstorm":
+                GlassMaterial.SetTexture("_MainTex", TextureRainDay);
+                break;
+        }
+    }
+}
