44eb2cf88f64846b154424ef17a099cf009f26be
ytakashina <takashina2051@gmail.com>
2019-03-12 16:20:59 +0900

    Fixed weatherfetcher

--- Assets/WeatherFetcher.cs
+++ Assets/WeatherFetcher.cs
@@ -1,53 +1,35 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Newtonsoft.Json;
-using System.IO;
-using System.Net;
+﻿using System.Collections;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 public class WeatherFetcher : MonoBehaviour
 {
-    public string ApiKeyWeather = "774233a13a975bc8fcd4b6e894482ce9";
-    public string ApiKeyGeoIp = "420786de36a519f50bdd36ea19c3d8d1a470ace60a6a8e764ac70e20";
-    public string City = "Tokyo";
-    public string Country = "Japan";
+    public TextAsset ConfigFile;
     public string Weather;
     public string Latitude;
     public string Longtitude;
 
-    string SendHttpRequest(string uri)
-    {
-        var request = (HttpWebRequest) WebRequest.Create(uri);
-        using (var response = (HttpWebResponse) request.GetResponse())
-        using (var stream = response.GetResponseStream())
-        using (var reader = new StreamReader(stream))
-        {
-            if ((int) response.StatusCode != 200)
-            {
-                throw new Exception($"Bad status code: {response.StatusCode}.");
-            }
-            return reader.ReadToEnd();
-        }
-    }
-
     // Use this for initialization
-    void Start()
+    IEnumerator Start()
     {
-        var uriGeoIp = $"https://api.ipdata.co/?api-key={ApiKeyGeoIp}";
-        var jsonGeoIp = SendHttpRequest(uriGeoIp);
-        var dictGeoIp = JObject.Parse(jsonGeoIp);
-        Latitude = (string) dictGeoIp["latitude"];
-        Longtitude = (string) dictGeoIp["longtitude"];
+        const string uriGeolocation = "https://ipinfo.io/json";
+        using (var www = new WWW(uriGeolocation))
+        {
+            yield return www;
+            var dictGeolocation = JObject.Parse(www.text);
+            var location = ((string) dictGeolocation["loc"]).Split(',');
+            Latitude = location[0];
+            Longtitude = location[1];
+        }
 
-        Debug.Log("executed");
-
-//        var uriWeather = $"http://api.openweathermap.org/data/2.5/weather?q={City},{Country}&appid={ApiKeyWeather}";
-        var uriWeather = $"http://api.openweathermap.org/data/2.5/weather?lat={Latitude}&lon={Longtitude}&appid={ApiKeyWeather}";
-        var jsonWeather = SendHttpRequest(uriWeather);
-        var dictWeather = JObject.Parse(jsonWeather);
-        Weather = (string) dictWeather["weather"][0]["main"];
+        var apiKeyWeather = JObject.Parse(ConfigFile.text)["ApiKeyWeather"];
+        var uriWeather = $"http://api.openweathermap.org/data/2.5/weather?lat={Latitude}&lon={Longtitude}&appid={apiKeyWeather}";
+        using (var www = new WWW(uriWeather))
+        {
+            yield return www;
+            var dictWeather = JObject.Parse(www.text);
+            Weather = (string) dictWeather["weather"][0]["main"];
+        }
     }
 
 
